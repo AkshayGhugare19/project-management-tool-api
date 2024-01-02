2d4ae8f7780f9ba1b104db233a8bfe7e3e84b801
AkshayGhugare19 <114797466+AkshayGhugare19@users.noreply.github.com>
2024-01-02 09:57:39 +0530

    task route add

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  profilePic:{ type: String, required: true },
   phoneNumber:{type: Number,required: true},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
